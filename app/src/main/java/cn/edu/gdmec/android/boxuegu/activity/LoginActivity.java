f6771c10abe9e1bee9e1fbaed458e53932c92b8f
Vapor1997 <893201500@qq.com>
2017-12-30 12:19:19 +0800

    //566655

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.boxuegu.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ * Created by student on 17/12/27.
+ */
+
+
+public class LoginActivity {
+
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/view/CourseView.java
@@ -0,0 +1,9 @@
+package cn.edu.gdmec.android.boxuegu.view;
+
+/**
+ * Created by student on 17/12/27.
+ */
+
+public class CourseView {
+    public static final int MSG_AD_SLID=002;
+}
